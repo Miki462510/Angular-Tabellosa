d5d1f92985d3f03731e75366dfc4c7a02f210279
Bruh Git Hub <78083568+Miki462510@users.noreply.github.com>
2022-06-08 13:51:51 +0200

    Add files via upload

---
+++ learn-table/learn-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learn-table',
+  templateUrl: './learn-table.component.html',
+  styleUrls: ['./learn-table.component.css']
+})
+export class LearnTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
